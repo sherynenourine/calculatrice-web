2c4ae6f5aeb48fb865b0d20cd2b3257edd776720
RABOULI <raboulisara@gmail.com>
2025-10-17 16:15:07 +0200

    edit Multi table

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const display = document.getElementById("display");
-  const buttons = document.getElementById("buttons");
-
-  const symbols = ["7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "0", "C", "=", "/"];
-  symbols.forEach(sym => {
-    const btn = document.createElement("button");
-    btn.textContent = sym;
-    buttons.appendChild(btn);
-  });
-
-  console.log("Calculatrice initialisée !");
-});
+export function multiply(a, b) {
+  return a * b;
+}
